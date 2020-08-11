f264d7f45bb5c51d12c1bc8847fb82fb2c83e748
Gabriel Dario <gzambranoa@yahoo.com>
2020-08-11 13:38:35 -0400

    agregando 1:23

--- condicionales.sh
+++ condicionales.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+#Este es el ultimo 1:23
 DISTRO1="DEBIAN"
 DISTRO2="UBUNTU"
-if [[ "$DISTRO1" == "$DISTRO2" ]]; then
+if [[ "$DISTRO1" = "$DISTRO2" ]]; then
 	echo "son iguales man"
 else
 	echo "SON DIFERENTES"
 fi
+
+echo "Este es el ultimo 1:23"
