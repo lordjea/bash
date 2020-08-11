12394ebf53bd4f1eebd05292f2d20e697aa8c277
Gabriel Dario <gzambranoa@yahoo.com>
2020-08-11 16:37:20 -0400

    Tra bajando con archivos

--- operadores_logicos.sh
+++ operadores_logicos.sh
@@ -14,21 +14,52 @@ cadena1=""
 #	echo "falso"
 #fi
 
-echo "Cadena1: $cadena1"
+#echo "Cadena1: $cadena1"
 
-if [[ -z $cadena1 ]]; then
-	echo "1: La cadena NO esta vacia"
+#if [[ -z $cadena1 ]]; then
+#	echo "1: La cadena NO esta vacia"
+#else
+#	echo "1: CADENA VACIA"
+#fi
+
+#if [[ -z $cadena1 ]]; then
+#	echo "2: La cadena esta vacia"
+#else
+#	echo "2: CADENA LLENA"
+#fi
+#if [[ $cadena1 = "" ]]; then
+#	echo "Cadena vacia convencional"
+#else
+#	echo "Cadena llena convencional"
+#fi
+
+# Comprobacion de atributos de ficheros
+
+# -d fichero existe y es un directorio
+# -e fichero existe
+# -f fichero existe y no es un directorio
+# -o eres dueño del fichero
+# -nt mas reciente que
+# -ot mas antiguo que
+
+
+directorio="/home/gabriel/Documents"
+
+if [[ -d $directorio ]]; then 
+	echo "Es un directorio"
 else
-	echo "1: CADENA VACIA"
+	echo "No es un directorio"
 fi
 
-if [[ -z $cadena1 ]]; then
-	echo "2: La cadena esta vacia"
-else
-	echo "2: CADENA LLENA"
-fi
-if [[ $cadena1 = "" ]]; then
-	echo "Cadena vacia convencional"
-else
-	echo "Cadena llena convencional"
-fi
+
+
+
+
+
+
+
+
+
+
+
+
