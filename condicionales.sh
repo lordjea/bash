52ad44f4404abaf703dbea7e62769fe25a4339b0
Gabriel Dario <gzambranoa@yahoo.com>
2020-08-11 13:07:34 -0400

    Agregando variables

--- condicionales.sh
+++ condicionales.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-if [[ "debian" = "debian2" ]]; then
+DISTRO1="DEBIAN"
+DISTRO2="UBUNTU"
+if [[ "$DISTRO1" == "$DISTRO2" ]]; then
 	echo "son iguales man"
 else
 	echo "SON DIFERENTES"
