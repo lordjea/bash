9aaecf0764f0a8dca8d080473ef63f7ecc504199
Gabriel Dario <gzambranoa@yahoo.com>
2020-08-11 13:53:13 -0400

    Sin doble corchete un solo igual

--- condicionales.sh
+++ condicionales.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 DISTRO1="DEBIAN"
-DISTRO2="UBUNTU"
-if [[ "$DISTRO1" = "$DISTRO2" ]]; then
+DISTRO2="DxxxxxEBIAN"
+if [ "$DISTRO1" = "$DISTRO2" ]; then
 	echo "son iguales man"
 else
 	echo "SON DIFERENTES"
 fi
 
-echo "Este es el ultimo 1:40 sin parentesis"
+echo "Este es el ultimo debian differentes 1:52"
