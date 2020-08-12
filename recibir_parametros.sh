7621699fa2506aa253b93c1c62363e164d285817
gzambranoa <gzambranoa@users.noreply.github.com>
2020-08-12 18:57:26 -0400

    Backup de un directorio recibiendo parametros
    Extension tgz

--- recibir_parametros.sh
+++ recibir_parametros.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
 
+#if [ -z "$1" ]; then
+#	echo "No se recibió un parámetro"
+#else
+#	echo "El parámetro es: $1"
+#if
+
+echo "Inicio del backup"
+
 if [ -z "$1" ]; then
 	echo "No se recibió un parámetro"
+	exit
+elif [ -d "$1" ]; then
+	echo "Se ingreso el directorio: $1"
 else
-	echo "El parámetro es: $1"
+	echo "El parámetro no corresponde a un directorio"
+	exit
 fi
+
+echo "Como se llamará tu respaldo?"
+read nombre_respaldo
+
+#Invocaremos a la utilidad tar para comprimir nuestro respaldo
+
+tar -czf "$nombre_respaldo.tgz" $1
+echo "Respaldo realizado exitosamente en $nombre_respaldo.tgz"
