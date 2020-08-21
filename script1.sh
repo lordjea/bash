43d9c0918258803e9421c281cfa27fe9c90008db
lordjea <lordjea@yahoo.com.mx>
2020-08-21 17:02:10 +0200

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,16 @@
+Crear un script para ponerle color a los mensajes datos
+
+Parametros obligatorios: 
+1- Color
+yellow
+blue
+cyan
+red
+white
+
+2- Mensaje
+
+Ejemplo:
+Input: yellow "este es el mensaje"
+Output las letras del mensaje deben salir en color amarillo: "este es el mensaje"
+
